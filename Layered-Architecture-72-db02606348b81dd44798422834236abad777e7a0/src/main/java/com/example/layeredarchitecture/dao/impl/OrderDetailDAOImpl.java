d0a5f82bc5a70300b8416a62bb91edca500780e7
AhasnaCharumee <charumeeahasna@gmail.com>
2025-01-28 14:33:56 +0530

    OrderDetailDAOImpl-remove boilerplate code

--- Layered-Architecture-72-db02606348b81dd44798422834236abad777e7a0/src/main/java/com/example/layeredarchitecture/dao/impl/OrderDetailDAOImpl.java
+++ Layered-Architecture-72-db02606348b81dd44798422834236abad777e7a0/src/main/java/com/example/layeredarchitecture/dao/impl/OrderDetailDAOImpl.java
@@ -1,5 +1,6 @@
 package com.example.layeredarchitecture.dao.impl;
 
+import com.example.layeredarchitecture.dao.SQLUtil;
 import com.example.layeredarchitecture.dao.custom.OrderDetailDAO;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.OrderDetailDTO;
@@ -11,12 +12,13 @@ import java.sql.SQLException;
 public class OrderDetailDAOImpl implements OrderDetailDAO {
     @Override
     public int addOrderdetail(String orderId, OrderDetailDTO detail) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
-        stm.setString(1, orderId);
-        stm.setString(2, detail.getItemCode());
-        stm.setBigDecimal(3, detail.getUnitPrice());
-        stm.setInt(4, detail.getQty());
-        return stm.executeUpdate();
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        PreparedStatement stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
+//        stm.setString(1, orderId);
+//        stm.setString(2, detail.getItemCode());
+//        stm.setBigDecimal(3, detail.getUnitPrice());
+//        stm.setInt(4, detail.getQty());
+//        return stm.executeUpdate();
+        return SQLUtil.execute("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)",orderId,detail.getItemCode(),detail.getUnitPrice(),detail.getQty());
     }
 }
